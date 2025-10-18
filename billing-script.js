424320e15fb0fce953c8d0a201cfdbd26adf1d6c
WebtorStudios <webtorstudios@gmail.com>
2025-10-18 20:28:23 +0200

    Update billing-script.js

--- billing-script.js
+++ billing-script.js
@@ -118,7 +118,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 price.textContent = `€${newPrice}`;
             });
             
-            const periodText = period === 'annual' ? 'per 6 mesi' : '.';
+            const periodText = period === 'annual' ? 'per 6 mesi' : '';
             periodTexts.forEach(text => {
                 text.textContent = periodText;
             });
@@ -247,5 +247,6 @@ async function checkUserStatus() {
   }
 
 }
+
 
 
